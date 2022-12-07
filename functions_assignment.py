d2bcc61554f51d308065ffdeff3c8bafd9bbe172
Kevin <git@kevinb4.com>
2022-12-07 16:01:48 -0500

    Fixed Bugs/Compacted Code in Functions Assignment

--- functions_assignment.py
+++ functions_assignment.py
@@ -1,5 +1,6 @@
 def bad_char_check(passed_input, type):
     passed = False
+    list = []
 
     if type == "name":
         # setup custom char filter - add numbers since a name cannot contain numbers because using isalpha would return false if the user has a period in their name, which isn't on the special characters list
@@ -12,8 +13,8 @@ def bad_char_check(passed_input, type):
         # setup custom char filter
         list = ['!', '"', '\'', '@', '$', '%', '^', '&', '*', '_', '=', '+', '<', '>', '?', ';', ':', '[', ']', '{', '}']
     else: # this shouldn't happen but it's good to have a safety net
-        print("Failed to get illegal character filters")
-        return
+        print("Failed to get illegal character list")
+        return False
 
     for char in passed_input:
         if char in list:
@@ -24,86 +25,53 @@ def bad_char_check(passed_input, type):
             
     return passed
 
-def get_id():
-    id = input("Please enter your employee ID: ")
-    id_ok = False
+def get_data(type):
+    msg = f"Please enter your employee {type}"
 
-    while not id_ok:
-        if not id: # no user entry
-            id_ok = False
-        else:
-            if id.isdigit() and len(id) <= 7: # make sure input is a valid number and that it's 7 or less digits
-                id_ok = True
+    # inform the user address is optional when prompted
+    if type == "address":
+        msg = f"{msg} (optional): "
+    else:
+        msg = f"{msg}: "
+
+    data = input(msg) # get data
+    data_ok = False
+
+    while not data_ok:
+        if not data and type != "address": # handle nothing entered as long as it's not the address
+            data_ok = False
+        elif not data and type == "address": # handle the optional address
+            data_ok = True
+        elif type == "id": # special handler for id since it doesn't use the char checker
+            if data.isdigit() and len(data) <= 7: # make sure input is a valid number and that it's 7 or less digits
+                data_ok = True
             else:
-                id_ok = False
-
-        if not id_ok:
-            id = input("The ID you entered is invalid, please enter a seven digit ID: ")
-
-    return id
-
-def get_name():
-    name = input("Please enter your employee name: ")
-    name_ok = False
-
-    while not name_ok:
-        if name:
-            name_ok = bad_char_check(name, "name")
-        else: # no user entry
-            name_ok = False
-
-        if not name_ok:
-            name = input("The name you entered is not valid, please enter a name with alphabet characters only: ")
-
-    return name
-
-def get_email():
-    email = input("Please enter your email: ")
-    email_ok = False
-
-    while not email_ok:
-        if email:
-            email_ok = bad_char_check(email, "email")
-        else: # no user entry
-            email_ok = False
-
-        if not email_ok:
-            email = input("The email you entered is not valid, please enter an email only using alphanumeric characters and the special characters \"@\" and \".\": ")
-
-    return email
-
-def get_address():
-    address = input("Please enter your address (optional): ")
-    address_ok = False
-
-    while not address_ok:
-        if not address: # to skip address
-            address_ok = True
+                data_ok = False
         else:
-            address_ok = bad_char_check(address, "address")
+            data_ok = bad_char_check(data, type)
 
-        if not address_ok:
-            address = input("The address you entered is not valid, please enter an address only using special characters in addresses: ")
+        if not data_ok:
+            data = input(f"The {type} you entered is not valid, please enter characters only used in a {type}: ")
 
-    return address
+    return data
 
 cont = True
 employees = []
 
 while cont:
-    id = get_id()
-    name = get_name()
-    email = get_email()
-    address = get_address()
+    id = get_data("id")
+    name = get_data("name")
+    email = get_data("email")
+    address = get_data("address")
 
     # make sure to only include addresses if the user entered one
     if address:
-        employees.append({ 'id': id, 'name': name, 'email': email, 'address': address})
+        employees.append({ 'id': id, 'name': name, 'email': email, 'address': address })
     else:
-        employees.append({ 'id': id, 'name': name, 'email': email})
+        employees.append({ 'id': id, 'name': name, 'email': email })
 
     # check to make sure the max amount of employees hasn't been reached
-    if len(employees) <= 5:
+    if len(employees) < 5:
         response = input("Add another employee? (Y/N): ")
         if response.lower() == "n":
             cont = False # we only need to set continue to false since it's already true
